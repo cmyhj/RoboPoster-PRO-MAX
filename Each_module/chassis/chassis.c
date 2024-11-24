710e0549c6bf454130949c36389c516ce79ffb73
wdsxzc <2046598085@qq.com>
2024-11-24 13:12:55 +0800

    eg1_1

--- Each_module/chassis/chassis.c
+++ Each_module/chassis/chassis.c
@@ -9,8 +9,8 @@ void Chassis_Task(void const * argument)
   /* Infinite loop */
   for(;;)
   {
-//	CAN_cmd_chassis(0,0,0,0);
-	CAN_cmd_gimbal(V_yaw,V_r,0, 0);
+	CAN_cmd_chassis(Motor_measure_2006[0].Output,Motor_measure_2006[1].Output,Motor_measure_2006[2].Output,Motor_measure_2006[3].Output);
+	CAN_cmd_gimbal(Motor_measure_6020.set_voltage,Motor_measure_2006[4].Output,0, 0);
 	Servo_Set();
     osDelay(1);
   }
@@ -19,8 +19,11 @@ void Chassis_Task(void const * argument)
 
 void PID_Task(void const * argument)
 {
-	Motor_measure_2006[4].Output=pid_calc(&M2006_speed_pid[4],pid_calc(&M2006_angle_pid[4],A_r,Motor_measure_2006[4].total_ecd),Motor_measure_2006[4].speed_rpm);
-    Motor_measure_6020.set_voltage=pid_calc(&M6020_yaw_speed_pid, pid_calc(&M6020_yaw_angle_pid, A_yaw*22.7528, Motor_measure_6020.rotor_angle), Motor_measure_6020.rotor_speed);
+	for(int i = 0;i++;i<5)
+	{
+		Motor_measure_2006[i].Output=pid_calc(&M2006_speed_pid[i],pid_calc(&M2006_angle_pid[i],A_r,Motor_measure_2006[i].total_ecd),Motor_measure_2006[i].speed_rpm);
+    }
+	Motor_measure_6020.set_voltage=pid_calc(&M6020_yaw_speed_pid, pid_calc(&M6020_yaw_angle_pid, A_yaw*22.7528, Motor_measure_6020.rotor_angle), Motor_measure_6020.rotor_speed);
 	osDelay(40);
 }
 
